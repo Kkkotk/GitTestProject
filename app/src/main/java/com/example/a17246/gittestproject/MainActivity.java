09d1daf952f58989a4be2653c2b56da1848aa685
testName <test@qq.com>
2018-01-23 11:50:03 +0800

    添加按钮

--- app/src/main/java/com/example/a17246/gittestproject/MainActivity.java
+++ app/src/main/java/com/example/a17246/gittestproject/MainActivity.java
@@ -2,12 +2,14 @@ package com.example.a17246.gittestproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button mButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mButton = (Button) findViewById(R.id.activity_main_bt_test);
     }
 }
