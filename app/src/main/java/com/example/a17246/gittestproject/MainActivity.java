5c1bb2c2b524dd1af7f1ef2b9df64d31664ed48b
testName <test@qq.com>
2018-01-23 13:21:38 +0800

    添加监听器

--- app/src/main/java/com/example/a17246/gittestproject/MainActivity.java
+++ app/src/main/java/com/example/a17246/gittestproject/MainActivity.java
@@ -2,9 +2,10 @@ package com.example.a17246.gittestproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private Button mButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +15,10 @@ public class MainActivity extends AppCompatActivity {
         /**
          * 测试文件
          */
+    }
+
+    @Override
+    public void onClick(View v) {
+        
     }
 }
